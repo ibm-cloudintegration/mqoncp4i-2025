76ae94b7ec3e1191cc9f5dba11503b1e91d485b1
JOSEPH JODL <jjodl@us.ibm.com>
2025-04-27 12:09:00 -0500

    Update getMessage.sh

--- MQonCP4I/unicluster/test1/test1/getMessage.sh
+++ MQonCP4I/unicluster/test1/test1/getMessage.sh
@@ -7,5 +7,5 @@ export MQAPPLNAME='MY.PUTTER.APP'
 export MQCCDTURL='/home/ibmuser/MQonCP4I/unicluster/test1/ccdt.json'
 export MQSSLKEYR='/home/ibmuser/MQonCP4I/unicluster/test1/key'
 CCDT_NAME=${2:-"*ANY_QM"}
-echo "Starting amqsghac" $CCDT_NAME
+echo "Starting amqsghac" $CCDT_NAME 
 /opt/mqm/samp/bin/amqsghac APPQ $CCDT_NAME

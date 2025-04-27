7ba11b05dbc2e8dcba28c3eb989be81ca3f358d4
JOSEPH JODL <jjodl@us.ibm.com>
2025-04-27 12:14:00 -0500

    Update getUni.sh

--- MQonCP4I/unicluster/test1/getUni.sh
+++ MQonCP4I/unicluster/test1/getUni.sh
@@ -10,7 +10,7 @@ export TARGET_NAMESPACE=cp4i-mq
 export QMpre=mq01
 export QMname=mq01a
 export CHLCAPS=MQ01CHLA
-export APPQ=APPQ
+export APPQ=APPQ 
 
 export HOST="$(oc get route $QMname-ibm-mq-qm -n $TARGET_NAMESPACE -o jsonpath='{.spec.host}')"
 ( echo "cat <<EOF" ; cat ccdt_template.json ; echo EOF ) | sh > ccdt_generated.json

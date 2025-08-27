bd925cf208750e242140925cc2a1410b80e571bb
JOSEPH JODL <jjodl@us.ibm.com>
2025-08-27 17:12:34 -0500

    automate test scripts

--- MQonCP4I/set_properties.sh
+++ MQonCP4I/set_properties.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 #
+###############################################################################################
+# Author: Joe Jodl
+# Date Created: 1/25/2025
+# Descripton: Set the Namespace in the properties file
+###############################################################################################
+#
 export TARGET_NAMESPACE=$1
 echo "[INFO] Update ${bold}setup.properties${normal} with your student number"
 ( echo 'cat <<EOF' ; cat setup.properties_template ; echo EOF ) | sh > setup.properties

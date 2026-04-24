22c4c371450779d3d407f0b54ca759a7f28ae9b3
JOSEPH JODL <jjodl@us.ibm.com>
2026-04-24 15:36:01 -0500

    Update set_properties.sh

--- MQonCP4I/set_properties.sh
+++ MQonCP4I/set_properties.sh
@@ -12,12 +12,12 @@ echo "[INFO] Generating setup.properties"
 echo "[INFO] Update ${bold}setup.properties${normal} with your student number"
 
 if [ $STUDENT_NUM -lt 11 ]; then
-  export OCP_CLUSTER1="https://api.itz-gwohoa.infra01-lb.dal14.techzone.ibm.com:6443"
+  export OCP_CLUSTER1="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 else
-  export OCP_CLUSTER1="https://api.itz-6uta2x.infra01-lb.dal14.techzone.ibm.com:6443"
+  export OCP_CLUSTER1="yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"
 fi
 #
-export OCP_CLUSTER2=https://api.itz-rz9v1d.infra01-lb.dal14.techzone.ibm.com:6443
+export OCP_CLUSTER2="zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"
 #
 envsubst < setup.properties_template > setup.properties
 
